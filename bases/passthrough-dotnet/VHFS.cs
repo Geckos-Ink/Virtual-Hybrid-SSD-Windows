fca6dd99b8e6d7ab7a2d20c867219166718964ac
Riccardo Cecchini <rcecchini.ds@gmail.com>
2023-10-26 23:10:22 +0200

    vhfs

---
+++ bases/passthrough-dotnet/VHFS.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VHSSD
+{
+    // Virtual Hybrid File System
+    public class VHFS
+    {
+
+        public class File
+        {
+            public VHFS fs;
+
+            public Int64 ID;
+
+            public string name;
+            public File parent;
+            public bool isDirectory;
+
+            // File
+            public Int64 length;
+
+            // Directory
+            public Dictionary<string, File> files; 
+        }
+    }
+}
