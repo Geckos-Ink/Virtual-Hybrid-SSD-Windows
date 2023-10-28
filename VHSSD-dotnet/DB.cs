dfc4fcb3c63afd5a56d4fc86dd533b95c8100c45
Riccardo Cecchini <rcecchini.ds@gmail.com>
2023-10-28 19:28:48 +0200

    hand made db

--- VHSSD-dotnet/DB.cs
+++ VHSSD-dotnet/DB.cs
@@ -1,8 +1,8 @@
-﻿using Sqlite.Fast;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,39 +11,104 @@ namespace VHSSD
     public class DB
     {
         VHFS vhfs;
-        Connection conn;
-
-        Statement<FS> stmtFSInsert;
+        string dir;
 
         public DB(VHFS vhfs) 
         {
             this.vhfs = vhfs;
 
             var dataDir = "data/";
-            if(!Directory.Exists(dataDir)) Directory.CreateDirectory(dataDir);  
+            if(!Directory.Exists(dataDir)) Directory.CreateDirectory(dataDir);
 
-            this.conn = new Connection(Path.Combine(dataDir, "db.db"));
-
-            /// Prepare statements
-            string sql = "insert into fs values (" +
-                "@id," +
-                "@parent," +
-                "@isDirectory," +
-                "@fileAttributes," +
-                "@grantedAccess," +
-                "@fileSize," +
-                "@allocationSize," +
-                "@creationTime," +
-                "@lastAccessTime," +
-                "@lastWriteTime," +
-                "@changeTime," +
-                "@securityDescription)";
-
-            stmtFSInsert = conn.CompileStatement<FS>(sql);
+            dir = dataDir + "dev/";
+            if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
         }
+
+        void InsertBytes(byte[] bytes)
+        {
+
+        }
+
+        class BytesTable
+        {
+            DB db;
+
+            public BytesTable(DB db, int size)
+            {
+                this.db = db;
+            }
+        }
+
+        class Table<T>
+        {
+            DB db;
+
+            Dictionary<string, MemberInfo> members = new Dictionary<string, MemberInfo>();
+
+            public Table(DB db)
+            {
+                this.db = db;
+
+                var type = typeof(T);
+
+                var members = type.GetMembers();
+                foreach(var member in members)
+                {
+                    this.members.Add(member.Name, member);
+                }
+            }
+
+            public void Insert (T row)
+            {
+
+            }
+        }
+
+        #region InternalStructs
+
+        [Serializable]
+        struct DataIndex
+        {
+            public ulong Index;
+            public ushort Size;
+        }
+
+        [Serializable]
+        struct Value
+        {
+            public byte[] Bytes;
+        }
+
+        [Serializable]
+        struct Keys
+        {
+            public Value[] OrderedKeys;
+            public ulong[] OrderedRowsIndexes;
+        }
+
+        [Serializable]
+        struct Row
+        {
+            public ulong Index;
+        }
+
+        [Serializable]
+        struct Values
+        {
+            public Value[] Data;
+        }
+
+        [Serializable]
+        struct Table
+        {
+            public DataIndex[] Columns;
+        }
+
+        #endregion
 
         #region Tables
 
+        [Serializable]
         public struct FS
         {
             public ulong ID;
